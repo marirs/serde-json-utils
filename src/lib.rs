78b0d8c02ffc409018cb9d74335a8ff6e5cafa1e
Andrey Mnatsakanov <andrey@3ig.kiev.ua>
2022-05-28 10:53:25 +0300

    add filtering empty arrays and empty maps

--- src/lib.rs
+++ src/lib.rs
@@ -8,24 +8,28 @@ pub enum Error {
 
 type Result<T> = std::result::Result<T, Error>;
 
-pub fn filter_nulls(val: &mut serde_json::Value) -> Result<()>{
+pub fn filter_nulls(val: &mut serde_json::Value) -> Result<bool>{
     match val{
-        serde_json::Value::Null => {}
+        serde_json::Value::Null => {return Ok(true);}
         serde_json::Value::Bool(_) => {}
         serde_json::Value::Number(_) => {}
         serde_json::Value::String(_) => {}
         serde_json::Value::Array(a) => {
+            if a.is_empty(){
+                return Ok(true);
+            }
             for v in a{
                 filter_nulls(v)?;
             }
         }
         serde_json::Value::Object(o) => {
+            if o.is_empty(){
+                return Ok(true);
+            }
             let mut candidates = vec![];
             for (k, v) in o.iter_mut() {
-                if let serde_json::Value::Null = v{
+                if filter_nulls(v)?{
                     candidates.push(k.to_string());
-                } else {
-                    filter_nulls(v)?;
                 }
             }
             for c in candidates{
@@ -33,7 +37,7 @@ pub fn filter_nulls(val: &mut serde_json::Value) -> Result<()>{
             }
         }
     }
-    Ok(())
+    Ok(false)
 }
 
 
@@ -66,11 +70,37 @@ mod tests {
 
 }
 "###;
-        let mut val: serde_json::Value = serde_json::from_str(test_str).unwrap();
 
-        super::filter_nulls(&mut val).unwrap();
+        let test_str2 = r###"{
+"adversary": "Double Guns",
+"category": "Malware",
+"description": "Recently, our DNS data based threat monitoring system DNSmon flagged a suspicious domain pro.csocools.com. The system estimates the scale of infection may well above hundreds of thousands of users. By analyzing the related samples and C2s,\n\nWe traced its family back to the ShuangQiang (double gun) campaign, in the past, this campaign has been exposed by multiple security vendors, but it has revived and come back with new methods and great force.",
+"submission_time": "2020-05-26 17:45:35 UTC",
+"indicator": "New activity of DoubleGuns Group, control hundreds of thousands of bots via public cloud service",
+"targeted_countries": {},
+"targeted_countriess": [],
+"intel": "Threat",
+"tlp": "white",
+"killchain": "Installation",
+"malware_families": [
+"Sakula - S0074",
+"Double-Gun"
 
-        println!("{:#?}", val);
+],
+"threat_type": "filehash",
+"sha256": "c323d49f16e6ad3a8f3f1ca78249385d703db2e33722476424ac3536f7043748"
+
+}
+"###;
+
+        let mut val1: serde_json::Value = serde_json::from_str(test_str).unwrap();
+        let mut val2: serde_json::Value = serde_json::from_str(test_str2).unwrap();
+
+        super::filter_nulls(&mut val1).unwrap();
+        println!("{:#?}", val1);
+
+        super::filter_nulls(&mut val2).unwrap();
+        println!("{:#?}", val2);
     }
 
 }
